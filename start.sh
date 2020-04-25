6b79a0d5a7ab2202d1e62deae326e3b5b9a4b9ba
chinglinwen <chinglinwen@gmail.com>
2020-04-25 13:28:32 +0000

    update start

--- start.sh
+++ start.sh
@@ -1,9 +1,8 @@
-pkill hugo
+set -x
 pkill githubhook
-
 ./githubhook &> hook.log &
 
 cd blogs/
-hugo server --bind 0.0.0.0 -b http://g.clwen.com/ --appendPort=false -D &
-# hugo server --bind 0.0.0.0 &
+# hugo server --bind 0.0.0.0 -b http://g.clwen.com/ --appendPort=false &
+./start.sh
 cd -
